ba78e39d439eba443d7b364d5cb3aaaa808aa720
jfrothin <jafroth@gmail.com>
2025-08-04 15:47:06 -0400

    plot basic receiver ranges

--- spectralplotter.py
+++ spectralplotter.py
@@ -14,7 +14,8 @@ from datetime import datetime
 import numpy as np
 import os
 
-#%% Set up data structures
+#%% Set up data structures - receiver ranges
+# receiver data downloaded from https://alda.gb.nrao.edu/receiver/receivers (internal GBO tool)
 
 rcvr_range_ghz_dict = {}
 
@@ -23,4 +24,19 @@ rcvr_data = np.loadtxt('receivers.csv', delimiter=',',skiprows=1, dtype=np.ndarr
 for i in range(0,len(rcvr_data)):
     rcvr_range_ghz_dict[rcvr_data[i,0]] = [float(rcvr_data[i,1]), float(rcvr_data[i,2])]
 
-print(rcvr_range_ghz_dict)
+#print(rcvr_range_ghz_dict)
+
+#%% Set up data structures - VEGAS modes
+ # to do later - will need to pull values from GBT Proposer's and Observer's Guides
+
+#%% Plotting
+
+toplot = rcvr_range_ghz_dict['Rcvr1_2']
+
+fig, ax = plt.subplots()
+
+for rcvr in rcvr_range_ghz_dict.keys():
+    x1, x2 = rcvr_range_ghz_dict[rcvr]
+    ax.fill_betweenx([0,1], x1, x2)
+
+ax.plot()
